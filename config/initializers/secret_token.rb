324cc1131542db5d520c351dd93f49b3ae3e03ed
rlew <rachel.lew@tufts.edu>
2011-07-26 22:04:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comp120A2::Application.config.secret_token = '273b06970320b8a6524b82cd68450f162f1ccee4fce664ecb253d423f8a83d3e287ab772df2aed12118fbc38fbc61563339ec087252dc78ca51b00a548d97b41'
